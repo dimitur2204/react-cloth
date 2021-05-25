cf4836b4301100e55afc336c5f909667af607795
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2021-05-25 10:05:11 +0300

    Added header markup in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
 	return (
-		<div className="App">
+		<div className="container">
+			<Header></Header>
 			<Switch>
 				<Route exact path="/" component={HomePage}></Route>
 				<Route exact path="/shop" component={ShopPage}></Route>
