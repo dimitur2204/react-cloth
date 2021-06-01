f9dc867a910c764647d4ac32e3f904894857f004
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2021-06-01 13:55:31 +0300

    Updated code to be pwa compliant

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,6 +7,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import * as serviceWorker from './serviceWorkerRegistration';
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -20,6 +21,8 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('root')
 );
+
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -13,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
@@ -29,6 +30,10 @@ app.listen(port, (err) => {
 		throw err;
 	}
 	console.log('Server running on: ', port);
+});
+
+app.get('/service-worker.js', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (req, res) => {
